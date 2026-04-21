cab50b5abb48673a5561ea482ae4f631eaa875ba
mar <105406641+birdyxs@users.noreply.github.com>
2026-04-21 17:53:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,26 @@
-var music = document.getElementById("bg-music");
+ var music = document.getElementById("bg-music");
     var icon = document.getElementById("music-icon");
-
-
-    window.addEventListener('click', () => {
-        if (music.paused) {
-            music.play();
-            icon.classList.remove("ph-play");
-            icon.classList.add("ph-pause");
-        }
-    }, { once: true });
+    var modal = document.getElementById("friends-modal");
 
     function toggleMusic() {
         if (music.paused) {
             music.play();
-            icon.classList.remove("ph-play");
-            icon.classList.add("ph-pause");
+            icon.classList.replace("ph-play", "ph-pause");
         } else {
             music.pause();
-            icon.classList.remove("ph-pause");
-            icon.classList.add("ph-play");
+            icon.classList.replace("ph-pause", "ph-play");
         }
     }
 
-document.addEventListener('contextmenu', event => event.preventDefault());
-							
-document.onkeydown = function (e) {
-							
-if(e.keyCode == 123) {
-return false;
-}
+    function toggleModal() {
+        modal.style.display = (modal.style.display === "flex") ? "none" : "flex";
+    }
 
-if(e.ctrlKey && e.shiftKey && e.keyCode == 73){
-return false;
-}
-							
-if(e.ctrlKey && e.shiftKey && e.keyCode == 74) {
-return false;
-}
-							
-if(e.ctrlKey && e.keyCode == 85) {
-return false;
-}
-}
+    function closeModalOutside(e) {
+        if (e.target === modal) toggleModal();
+    }
+
+    document.onkeydown = function(e) {
+        if(e.keyCode == 123 || (e.ctrlKey && e.shiftKey && (e.keyCode == 73 || e.keyCode == 74)) || (e.ctrlKey && e.keyCode == 85)) return false;
+    };
+    document.addEventListener('contextmenu', event => event.preventDefault());
